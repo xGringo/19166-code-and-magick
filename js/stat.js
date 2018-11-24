acfe78a1ee45820eb239bf00b608da3ba67f4ef8
xGringo <xlost2015@mail.ru>
2018-11-24 15:50:10 +0300

    добавил функции

--- js/stat.js
+++ js/stat.js
@@ -11,17 +11,21 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов: ', 120, 60);
 
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time.toFixed(0);
+  function getMaxValue(array) {
+    var max = -1;
+    for (var i = 0; i < array.length; i++) {
+      var value = array[i];
+      if (value > max) {
+        max = value;
+      }
     }
+    return max;
   }
 
   for (var i = 0; i < times.length; i++) {
+
     var histogramHeight = 150;
-    var step = histogramHeight / max;
+    var step = histogramHeight / getMaxValue(times);
     var columnHeight = times[i] * step;
     var columnWidth = 40;
     var initialX = 120;
@@ -29,21 +33,21 @@ window.renderStatistics = function (ctx, names, times) {
     var indent = 90;
     var indentName = 20;
     var indentTime = 15;
-    ctx.fillStyle = columnColor(names[i]);
+
+    ctx.fillStyle = (function (namePlayer, opacity) {
+      var namePlayer = names[i];
+      var opacity = Math.random().toFixed(1);
+      if (namePlayer === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, '+opacity+')';
+      }
+    }());
+
     ctx.fillRect(initialX + indent * i, initialY - columnHeight, columnWidth, columnHeight);
     ctx.fillStyle = '#000000';
     ctx.fillText(names[i], initialX + indent * i, initialY + indentName);
     ctx.fillText(times[i].toFixed(0), initialX + indent * i, initialY - columnHeight - indentTime);
 
   }
-  function columnColor(namePlayer, opacity) {
-    var namePlayer = names[i];
-    var opacity = Math.random().toFixed(1);
-    if (namePlayer === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, '+opacity+')';
-    }
-  }
-
 };
